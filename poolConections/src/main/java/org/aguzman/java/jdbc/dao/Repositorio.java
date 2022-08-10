22225e874cee48968e7540ba10305af452903254
Josafat Zavaleta <53987304+JosafatZG@users.noreply.github.com>
2022-08-10 11:25:23 -0600

    Add files via upload

---
+++ poolConections/src/main/java/org/aguzman/java/jdbc/dao/Repositorio.java
@@ -0,0 +1,9 @@
+package org.aguzman.java.jdbc.dao;
+import java.util.List;
+
+public interface Repositorio<T> {
+    List<T> listar();
+    T porId(Long id);
+    void guardar(T t);
+    void eliminar(Long id);
+}
